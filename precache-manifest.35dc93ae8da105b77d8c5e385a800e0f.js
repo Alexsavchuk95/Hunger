02c728469730687cc76ae682ae42a5ade2018d0d
AlexSavchuk95 <savchukalex57@gmail.com>
2020-11-26 17:42:19 +0200

    Updates

---
+++ precache-manifest.35dc93ae8da105b77d8c5e385a800e0f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4a168c723774e3954972336f33d545",
+    "url": "/Hunger/index.html"
+  },
+  {
+    "revision": "2fb3e889077d17dca671",
+    "url": "/Hunger/static/css/main.a269624c.chunk.css"
+  },
+  {
+    "revision": "c315240ec9f2b10c105f",
+    "url": "/Hunger/static/js/2.cd2ff37d.chunk.js"
+  },
+  {
+    "revision": "2fb3e889077d17dca671",
+    "url": "/Hunger/static/js/main.7a62b8e5.chunk.js"
+  },
+  {
+    "revision": "7a676217d7f591f31959",
+    "url": "/Hunger/static/js/runtime-main.0ff0429b.js"
+  },
+  {
+    "revision": "69419cd1836ab6c1d2f7b0714d6a3f0f",
+    "url": "/Hunger/static/media/1.69419cd1.png"
+  },
+  {
+    "revision": "746ffb9f09782568a7af351ecc25eb87",
+    "url": "/Hunger/static/media/2.746ffb9f.png"
+  },
+  {
+    "revision": "d42bb92db30d4c5d9c408cd3ec6f2fca",
+    "url": "/Hunger/static/media/3.d42bb92d.png"
+  },
+  {
+    "revision": "4a3ab09914af33279fa0d4a2a414e97a",
+    "url": "/Hunger/static/media/Banny-BoldRound.4a3ab099.woff"
+  },
+  {
+    "revision": "be8ce3374ee45f3cc9c0d1911afc2ff6",
+    "url": "/Hunger/static/media/Banny-LightRound.be8ce337.woff"
+  },
+  {
+    "revision": "4e4e2c5627e01c68fc8dda8a65445733",
+    "url": "/Hunger/static/media/Banny-Round.4e4e2c56.woff"
+  },
+  {
+    "revision": "7047777af6e5ff93ea90843703e57666",
+    "url": "/Hunger/static/media/about.7047777a.png"
+  },
+  {
+    "revision": "6ca8cefa4a89436ffbe2a8a8f4bb4cde",
+    "url": "/Hunger/static/media/headerBg.6ca8cefa.png"
+  },
+  {
+    "revision": "60c9839c392b6af2093da18695c45b00",
+    "url": "/Hunger/static/media/specBg.60c9839c.png"
+  }
+]);
